2ac6be7787aa70a344e6fa3b73902b7e4caafc90
Anatol Belski <ab@php.net>
2016-10-23 16:43:53 +0000

    prepare release 1.2.2
    git-svn-id: http://svn.php.net/repository/pecl/varnish/trunk@340608 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_varnish.h
+++ php_varnish.h
@@ -51,7 +51,7 @@ typedef long zend_long;
 #endif
 /*#define PHP_VARNISH_DEBUG*/
 
-#define PHP_VARNISH_VERSION "1.2.1"
+#define PHP_VARNISH_VERSION "1.2.2"
 
 PHP_MINIT_FUNCTION(varnish);
 PHP_MSHUTDOWN_FUNCTION(varnish);

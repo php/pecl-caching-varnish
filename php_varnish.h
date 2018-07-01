d30c5df0ca06ca4d6a42b355b3dbecb716cbe222
Anatol Belski <ab@php.net>
2018-07-01 11:30:53 +0000

    Back to dev
    git-svn-id: http://svn.php.net/repository/pecl/varnish/trunk@345257 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_varnish.h
+++ php_varnish.h
@@ -51,7 +51,7 @@ typedef long zend_long;
 #endif
 /*#define PHP_VARNISH_DEBUG*/
 
-#define PHP_VARNISH_VERSION "1.2.4"
+#define PHP_VARNISH_VERSION "1.2.5-dev"
 
 PHP_MINIT_FUNCTION(varnish);
 PHP_MSHUTDOWN_FUNCTION(varnish);
